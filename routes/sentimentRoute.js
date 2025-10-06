1189a3e9de77eab35a5424d1d4718df994c6682b
pranavsinghpatil <puneet.pranav04@gmail.com>
2025-10-07 00:33:35 +0530

    frontend up

--- routes/sentimentRoute.js
+++
@@ -1,8 +0,0 @@
-// routes/sentimentRoute.js
-import express from "express";
-import { analyzeSentiment } from "../controllers/sentimentController.js";
-const router = express.Router();
-
-router.post("/analyze", analyzeSentiment);
-
-export default router;
